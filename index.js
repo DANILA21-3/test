a7335ed72c168aef047602c664cd320621f64d75
DANILA21-3 <cherdanila184@gmail.com>
2025-04-03 18:31:29 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function loadData() {
             try {
                 var data = JSON.parse(request.responseText);
                 console.log("Загрузка выполнена успешно:", data);
-                displayCars(data);
                 simulateFalseError();
             }
             catch (error) {
@@ -21,16 +20,6 @@ function loadData() {
         console.error("Ошибка сети");
     };
     request.send();
-}
-function displayCars(data) {
-    data.forEach(function (car) {
-        console.log("\u041C\u0430\u0440\u043A\u0430: ".concat(car.data.make, ", \u041C\u043E\u0434\u0435\u043B\u044C: ").concat(car.data.model, ", \u0413\u043E\u0434: ").concat(car.data.year, ", \u042D\u043B\u0435\u043A\u0442\u0440\u043E\u043A\u0430\u0440: ").concat(car.data.is_electric ? "Да" : "Нет"));
-        console.log("Особенности:");
-        car.data.features.forEach(function (feature) {
-            console.log("- ".concat(feature));
-        });
-        console.log();
-    });
 }
 function simulateFalseError() {
     try {
